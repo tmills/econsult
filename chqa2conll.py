bbd46fc47682f166c66c309c636650ddf97ce8c3
tmill <tim.miller@gmail.com>
2018-10-13 06:01:56 -0400

    Initial check-in of files: convert chqa data to conll, train/eval chqa data with flair/glove, and python requirements.

---
+++ chqa2conll.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3.6
+
+from os.path import join
+import sys
+
+import nltk
+from nltk import pos_tag, word_tokenize
+
+def main(args):
+    if len(args) < 2:
+        sys.stderr.write('Two required argument: <chqa file> <output directory>\n')
+        sys.exit(-1)
+    
+    focus_out = open(join(args[1], 'focus.conll'), 'w')
+
+    import xml.etree.ElementTree as ET
+    tree = ET.parse(args[0])
+    root = tree.getroot()
+
+    for request in root.findall('Request'):
+        text = request.find('Text').text
+        tagged = pos_tag(word_tokenize(text))
+
+        focus = request.find('Focus')
+        if not focus is None:
+            focus_start = int(focus.attrib['start'])
+            focus_end = int(focus.attrib['len']) + focus_start
+            # print("Focus is: %s" % ( text[focus_start:focus_end] ) )
+        
+        start_ind = 0
+        prev_label = 'O'
+        for tag in tagged:
+            # tag[0] is the word and tag[1] is the POS
+            token_start_ind = text.find(tag[0], start_ind)
+            token_end_ind = token_start_ind + len(tag[0])
+            start_ind = token_end_ind
+            if focus_start == token_start_ind:
+                label = 'B-FOCUS'
+            elif (prev_label == 'B-FOCUS' or prev_label == 'I-FOCUS') and token_end_ind <= focus_end:
+                label = 'I-FOCUS'
+            else:
+                label = 'O'
+            focus_out.write('%s\t%s\t%s\n' % (tag[0], tag[1], label) )
+            prev_label = label
+
+        for question in request.findall('Question'):
+            qid = question.attrib['id']
+            q_start = int(question.attrib['start'])
+            q_end = int(question.attrib['len']) + q_start
+            q_text = text[q_start:q_end]
+            for sub_q in question.findall('SubQuestion'):
+                q_cat = sub_q.attrib['qt']
+
+                print('Question \'%s\' has type %s' % (q_text, q_cat))
+
+        focus_out.write('\n')
+    focus_out.close()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ flair_chqa.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3.6
+#
+#######################
+# flair_chqa.py
+# Train and evaluate the flair embeddings on the chqa
+# focus task. 
+# This code is borrowed from:
+# https://github.com/zalandoresearch/flair/blob/master/resources/docs/TUTORIAL_TRAINING_A_MODEL.md
+#######################
+
+import sys
+
+from flair.data import TaggedCorpus
+from flair.data_fetcher import NLPTaskDataFetcher, NLPTask
+from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings
+from typing import List
+
+# def read_data(data_dir):
+#     sentences_train = NLPTaskDataFetcher.read_conll_sequence_labeling_data(join(data_folder, 'train.txt') )
+#     # sample dev data from train
+#     sentences_dev: List[Sentence] = [sentences_train[i] for i in NLPTaskDataFetcher.__sample()]
+#     sentences_train = [x for x in sentences_train if x not in sentences_dev]
+#     sentences_test = NLPTaskDataFetcher.read_conll_sequence_labeling_data(join(data_folder, 'test.txt') )
+
+#     return TaggedCorpus(sentences_train, sentences_dev, sentences_test)
+
+def main(args):
+    if len(args) < 1:
+        sys.stderr.write('One required argument: <data dir>')
+        sys.exit(-1)
+    
+    # 1. get the corpus
+    column_format = {0:'word', 1:'pos', 2:'focus'}
+    corpus: TaggedCorpus = NLPTaskDataFetcher.fetch_column_corpus(args[0],
+        column_format,
+        'train.txt',
+        'test.txt',
+        'test.txt')
+    print(corpus)
+
+    # 2. what tag do we want to predict?
+    tag_type = 'focus'
+
+    # 3. make the tag dictionary from the corpus
+    tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
+    print(tag_dictionary.idx2item)
+
+    # 4. initialize embeddings
+    embedding_types: List[TokenEmbeddings] = [
+
+        WordEmbeddings('glove'),
+
+        # comment in this line to use character embeddings
+        # CharacterEmbeddings(),
+
+        # comment in these lines to use contextual string embeddings
+        # CharLMEmbeddings('news-forward'),
+        # CharLMEmbeddings('news-backward'),
+    ]
+
+    embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
+
+    # 5. initialize sequence tagger
+    from flair.models import SequenceTagger
+
+    tagger: SequenceTagger = SequenceTagger(hidden_size=256,
+                                            embeddings=embeddings,
+                                            tag_dictionary=tag_dictionary,
+                                            tag_type=tag_type,
+                                            use_crf=True)
+
+    # 6. initialize trainer
+    from flair.trainers import SequenceTaggerTrainer
+
+    trainer: SequenceTaggerTrainer = SequenceTaggerTrainer(tagger, corpus, test_mode=True)
+
+    # 7. start training
+    trainer.train('resources/taggers/chqa-focus',
+                learning_rate=0.1,
+                mini_batch_size=32,
+                max_epochs=150)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
