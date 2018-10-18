f38301516d2d7b59b48af1b4d1bf9b22df948d94
Tim Miller <tim.miller@gmail.com>
2018-10-18 07:49:49 -0400

    Fix column names and comment in CSL model

--- flair_train_chqa_focus.py
+++ flair_train_chqa_focus.py
@@ -12,7 +12,7 @@ import sys
 
 from flair.data import TaggedCorpus
 from flair.data_fetcher import NLPTaskDataFetcher, NLPTask
-from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings, CharacterEmbeddings
+from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings, CharacterEmbeddings, CharLMEmbeddings
 from typing import List
 
 
@@ -22,7 +22,7 @@ def main(args):
         sys.exit(-1)
     
     # 1. get the corpus
-    column_format = {0:'word', 1:'pos', 2:'focus'}
+    column_format = {0:'word', 1:'pos', 2:'ner'}
     corpus: TaggedCorpus = NLPTaskDataFetcher.fetch_column_corpus(args[0],
         column_format,
         'train.txt',
@@ -31,7 +31,7 @@ def main(args):
     print(corpus)
 
     # 2. what tag do we want to predict?
-    tag_type = 'focus'
+    tag_type = 'ner'
 
     # 3. make the tag dictionary from the corpus
     tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
@@ -43,11 +43,11 @@ def main(args):
         WordEmbeddings('glove'),
 
         # comment in this line to use character embeddings
-        CharacterEmbeddings(),
+        # CharacterEmbeddings(),
 
         # comment in these lines to use contextual string embeddings
-        # CharLMEmbeddings('news-forward'),
-        # CharLMEmbeddings('news-backward'),
+        CharLMEmbeddings('news-forward'),
+        CharLMEmbeddings('news-backward'),
     ]
 
     embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
@@ -64,10 +64,10 @@ def main(args):
     # 6. initialize trainer
     from flair.trainers import SequenceTaggerTrainer
 
-    trainer: SequenceTaggerTrainer = SequenceTaggerTrainer(tagger, corpus, test_mode=True)
+    trainer: SequenceTaggerTrainer = SequenceTaggerTrainer(tagger, corpus, test_mode=False)
 
     # 7. start training
-    trainer.train('resources/taggers/chqa-focus-glove+char',
+    trainer.train('resources/taggers/temp',
                 learning_rate=0.1,
                 mini_batch_size=32,
                 max_epochs=150)
