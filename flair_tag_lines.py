4e9d45bcc37caedaa6f22c5cf7962d6a4601150c
Tim Miller <tim.miller@gmail.com>
2018-11-09 09:17:14 -0500

    Fix tagger to correct periods and split on sentences, and do real tokenization.

--- flair_tag_lines.py
+++ flair_tag_lines.py
@@ -6,6 +6,8 @@ import sys
 
 from flair.data import Sentence
 from flair.models import SequenceTagger
+from nltk import word_tokenize, sent_tokenize
+import re
 
 def main(args):
     if len(args) < 1:
@@ -15,9 +17,13 @@ def main(args):
     tagger = SequenceTagger.load_from_file(args[0])
 
     for line in sys.stdin:
-        sentence = Sentence(line.rstrip())
-        tagger.predict(sentence)
-        print(sentence.to_tagged_string())
+        line = re.sub(r'([a-zA-Z])\.([a-zA-Z])', r'\1. \2', line.rstrip())
+
+        sentences = sent_tokenize(line.rstrip())
+        for sent_str in sentences:
+            sentence = Sentence(' '.join(word_tokenize(sent_str)))
+            tagger.predict(sentence)
+            print(sentence.to_tagged_string())
 
 if __name__ == '__main__':
     main(sys.argv[1:])
